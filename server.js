cc7cd9d1a382ffc4ead7b440c401c531f6f38414
C4Jay <choolakejay@gmail.com>
2020-03-14 12:17:00 +0530

    cpus-scores-cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const app = require('./app');
+const cors = require('cors')
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
 const server = http.createServer(app);
+
 
 server.listen(port);
